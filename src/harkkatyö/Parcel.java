5ba8c3f9d279664449fa05752d83f0b89deb8857
camilla <camilla@localhost.localdomain>
2016-07-21 17:13:14 +0300

    pakettiluokkaraakile

---
+++ src/harkkatyö/Parcel.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package harkkatyö;
+
+/**
+ *
+ * @author camilla
+ */
+public class Parcel{
+    
+    public Parcel(int id, String item, String size) {
+        
+        
+        
+    }
+    
+}
